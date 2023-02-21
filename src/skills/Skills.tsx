f3ee7281e4c89cafa71751e0ce6dfcf9767961c0
cros6y6raga <crosbybraga@gmail.com>
2023-02-21 10:00:03 +0300

    Adding props

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Skills.module.css';
 import styleContainer from '../common/styles/Container.module.css'
+import Skill from "./skill/Skill";
 
 function Skills() {
     return (
@@ -8,6 +9,9 @@ function Skills() {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}></div>
+                <Skill/>
+                <Skill/>
+                <Skill/>
             </div>
         </div>
     );
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import style from './Skill.module.css';
 
-const Skill = () => {
+function Skill(props: any) {
     return (
-        <div>
-            
+        <div className={style.skill}>
+            <div className={style.icon}></div>
+            <h3>{props.title}</h3>
         </div>
     );
-};
+}
 
 export default Skill;
