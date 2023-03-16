8af5c7fc792a06384dd6d4315263af7917f6ffe0
cros6y6raga <crosbybraga@gmail.com>
2023-03-16 12:24:00 +0300

    Analogy with skills

--- src/works/Works.tsx
+++ src/works/Works.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import style from './Works.module.css';
+import styleContainer from '../common/styles/Container.module.css'
+import Work from "./work/Work";
 
 function Works() {
     return (
-        <div>
-
+        <div className={style.worksBlock}>
+            <div className={`${styleContainer.container} ${style.worksContainer}`}>
+                <h2 className={style.title}>My works</h2>
+                <div className={style.works}>
+                    <Work/>
+                    <Work/>
+                </div>
+            </div>
         </div>
     );
 }
