d408c85f4af7cdd3c356cb635e9d06787b1316aa
cros6y6raga <crosbybraga@gmail.com>
2023-03-17 13:21:31 +0300

    Contacts styles (form and input)

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -7,11 +7,12 @@ function Contacts() {
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <h2 className={style.title}>Contacts</h2>
-                <form action="">
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea name="" id=""></textarea>
+                <form className={style.contactsForm} action="">
+                    <input className={style.contactsInput} type="text"/>
+                    <input className={style.contactsInput} type="text"/>
+                    <textarea className={style.contactsTextarea} name="" id=""></textarea>
                 </form>
+                <a href="">Отправить</a>
             </div>
         </div>
     );
