356d8c68cf61c170d842ed20d1d977dda185114f
cros6y6raga <crosbybraga@gmail.com>
2023-03-17 13:02:54 +0300

    Contacts+

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -6,7 +6,12 @@ function Contacts() {
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-
+                <h2 className={style.title}>Contacts</h2>
+                <form action="">
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea name="" id=""></textarea>
+                </form>
             </div>
         </div>
     );
