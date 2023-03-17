6966c2917823beece4155570ebb9f2d782c0df45
cros6y6raga <crosbybraga@gmail.com>
2023-03-17 12:22:58 +0300

    Basic code options

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./header/Header";
 import Main from "./main/Main";
 import Skills from "./skills/Skills";
 import Works from "./works/Works";
+import Options from "./options/Options";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Main/>
             <Skills/>
             <Works/>
+            <Options/>
         </div>
     );
 }
--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import style from "../options/Options.module.css";
+import styleContainer from "../common/styles/Container.module.css";
 
 function Options() {
     return (
-        <div></div>
+        <div className={style.optionsBlock}>
+            <div className={`${styleContainer.container} ${style.optionsContainer}`}>
+                <h2 className={style.title}>Considering remote work</h2>
+                <button className={style.hire}>Hire me</button>
+            </div>
+        </div>
     );
 }
 
