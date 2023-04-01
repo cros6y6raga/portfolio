dadf1f689ac2f89ad33d4a5caa8fbea39642b718
cros6y6raga <crosbybraga@gmail.com>
2023-04-01 09:37:17 +0300

    Added a Title component

---
+++ src/common/components/title/Title.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Title.module.css';
+
+function Title() {
+    return (
+        <div className={style.title}>
+            <h2>My works</h2>
+        </div>
+    );
+}
+
+export default Title;
--- src/works/Works.tsx
+++ src/works/Works.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import style from './Works.module.css';
 import styleContainer from '../common/styles/Container.module.css'
 import Work from "./work/Work";
+import Title from "../common/components/title/Title";
 
 function Works() {
     return (
         <div className={style.worksBlock}>
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
-                <div className={style.title}>
-                    <h2>My works</h2>
-                </div>
+                <Title/>
                 <div className={style.works}>
                     <Work title={'Task-list'}
                           description={'It is a long established fact that a reader will be distracted by the readable ' +
