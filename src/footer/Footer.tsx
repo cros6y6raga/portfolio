0a7e79326e1249950c45fd8fb7987a52c0e07330
cros6y6raga <crosbybraga@gmail.com>
2023-04-17 10:56:58 +0300

    Ref: Footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -11,7 +11,6 @@ function Footer() {
                     <div className={style.social}></div>
                     <div className={style.social}></div>
                     <div className={style.social}></div>
-                    <div className={style.social}></div>
                 </div>
             </div>
         </div>
