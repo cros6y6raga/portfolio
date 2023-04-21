11d392cc1c40e15c4e4d9ee58f753fb5789a849b
cros6y6raga <crosbybraga@gmail.com>
2023-04-21 11:37:12 +0300

    Added pictures

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,6 +3,10 @@ import style from './Skills.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
+import laptop from '../assets/image/com-laptop-code-svgrepo-com.svg'
+import warning from '../assets/image/con-warning-svgrepo-com (2).svg'
+import palette from '../assets/image/des-palette-svgrepo-com.svg'
+import bubble from '../assets/image/cha-bubble-female-svgrepo-com.svg'
 
 function Skills() {
     return (
@@ -11,13 +15,21 @@ function Skills() {
                 <Title text={'Skills'}/>
                 <div className={style.skills}>
                     <Skill title={'DEVELOPMENT'}
-                           description={'JavaScript, TypeScript, React, Redux, SCSS & CSS, Git'}/>
+                           description={'JavaScript, TypeScript, React, Redux, SCSS & CSS, Git'}
+                           images={laptop}
+                    />
                     <Skill title={'TESTING'}
-                           description={'Unit Tests, SnapShot, Storybook'}/>
+                           description={'Unit Tests, SnapShot, Storybook'}
+                           images={warning}
+                    />
                     <Skill title={'DESIGN'}
-                           description={'Material UI, Figma, Photoshop'}/>
+                           description={'Material UI, Figma, Photoshop'}
+                           images={palette}
+                    />
                     <Skill title={'SOCIAL'}
-                           description={'Well-coordinated teamwork, with a sense of humor, small talk, easy and quick to learn'}/>
+                           description={'Well-coordinated teamwork, with a sense of humor, small talk, easy and quick to learn'}
+                           images={bubble}
+                    />
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import style from './Skill.module.scss';
-import skillImage from '../../assets/image/com-laptop-code-svgrepo-com.svg';
+// import skillImage from '../../assets/image/com-laptop-code-svgrepo-com.svg';
 
 type PropsType = {
     title: string
     description: string
+    images:string
 }
 
 function Skill(props: PropsType) {
     return (
         <div className={style.skill}>
             <div className={style.icon}>
-                <img src={skillImage} alt=""/>
+                <img src={props.images} alt=""/>
             </div>
             <h3>{props.title}</h3>
             <span className={style.description}>
