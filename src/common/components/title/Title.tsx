ba78363cb8a571f5dbaf9683b38cbc64981ea135
cros6y6raga <crosbybraga@gmail.com>
2023-04-24 13:13:03 +0300

    Ref: Title

--- src/common/components/title/Title.tsx
+++ src/common/components/title/Title.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import style from './Title.module.scss';
 
-type PropsType={
-    text:string
+type PropsType = {
+    text: string
 }
 
-function Title(props:PropsType) {
+function Title(props: PropsType) {
     return (
         <div className={style.title}>
             <h2>{props.text}</h2>
