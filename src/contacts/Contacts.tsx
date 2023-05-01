aad2e34d548af8623850aec325681abf8e186a36
cros6y6raga <crosbybraga@gmail.com>
2023-05-01 09:17:51 +0300

    Added placeholder to form

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -8,9 +8,9 @@ function Contacts() {
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <h2 className={style.title}>Contacts</h2>
                 <form className={style.contactsForm} action="">
-                    <input className={style.contactsInput} type="text"/>
-                    <input className={style.contactsInput} type="text"/>
-                    <textarea className={style.contactsTextarea} name="" id=""></textarea>
+                    <input className={style.contactsInput} type="text" placeholder={'Name'}/>
+                    <input className={style.contactsInput} type="text" placeholder={'E-mail'}/>
+                    <textarea className={style.contactsTextarea} placeholder={'Message'}></textarea>
                 </form>
                 <a className={style.contactsSend} href="">SEND MESSAGE</a>
             </div>
