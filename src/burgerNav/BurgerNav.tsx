40e580aa070af989b2d7ef5a7e26bcd7c437cbc5
cros6y6raga <crosbybraga@gmail.com>
2023-05-13 10:48:04 +0300

    Created a burger menu

---
+++ src/burgerNav/BurgerNav.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import style from './BurgerNav.module.scss';
+import {Link, animateScroll as scroll} from "react-scroll";
+
+function BurgerNav() {
+    return (
+        <div className={style.burgerNav}>
+            <Link
+                activeClass={style.active}
+                to="main"
+                spy={true}
+                smooth={true}
+                offset={1}
+                duration={500}
+            >
+                Main</Link>
+            <Link
+                activeClass={style.active}
+                spy={true}
+                smooth={true}
+                offset={1}
+                duration={500}
+                to="skills">
+                Skills
+            </Link>
+            <Link
+                activeClass={style.active}
+                spy={true}
+                smooth={true}
+                offset={1}
+                duration={500}
+                to="works">
+                Works
+            </Link>
+            <Link
+                activeClass={style.active}
+                spy={true}
+                smooth={true}
+                offset={1}
+                duration={500}
+                to="contacts">
+                Contacts
+            </Link>
+        </div>
+    );
+}
+
+export default BurgerNav;
